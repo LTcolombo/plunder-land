8bde0a4c70d35fd1724d85035b83f9680270375d
Nick Balyanitsa <nick@sekai.gg>
2023-06-12 22:18:57 +0100

    added ethers+wallet connect

--- plunder-land-client/src/popups/gameenterpopup.ts
+++ plunder-land-client/src/popups/gameenterpopup.ts
@@ -1,24 +1,25 @@
 import { Container, Point, Sprite, Text, Texture } from 'pixi.js'
 import { TapHandler } from '../ui/taphandler'
 import AnimationClip from '../animation/animationclip'
+import { BrowserProvider, type JsonRpcSigner } from 'ethers'
 
 export default class GameEnterPopup extends Container {
   callback: () => void
   bg: Sprite
+  container: Container
 
   constructor (callback: () => void) {
     super()
     this.callback = callback
 
-    const container = new Container()
-    this.addChild(container)
+    this.container = new Container()
+    this.addChild(this.container)
 
     this.bg = new Sprite(Texture.from('UI/elements/BG.png'))
     this.bg.scale = new Point(2, 2)
-    console.log(this.bg.width)
-    container.x = -this.bg.width / 2
-    container.y = -this.bg.height / 2
-    container.addChild(this.bg)
+    this.container.x = -this.bg.width / 2
+    this.container.y = -this.bg.height / 2
+    this.container.addChild(this.bg)
 
     const cell = new Container()
 
@@ -33,28 +34,72 @@ export default class GameEnterPopup extends Container {
     player1.y = cell.height / 2
     cell.addChild(player1)
 
-    container.addChild(cell)
+    this.container.addChild(cell)
 
     cell.x = (this.bg.width - cell.width) / 2
     cell.y = (this.bg.height - cell.height) / 2
 
-    const startButton = new TapHandler(this.onStartClick.bind(this))
-
-    const buttonBg = new Sprite(Texture.from('UI/elements/button.png'))
-    startButton.addChild(buttonBg)
-    const label = new Text('START', { fill: '0xFFFFFF' })
-    label.anchor = new Point(0.5, 0.5)
-    label.position = new Point(buttonBg.width / 2, buttonBg.height / 2)
-    startButton.addChild(label)
-
-    container.addChild(startButton)
-
-    startButton.x = (this.bg.width - buttonBg.width) - 20
-
-    startButton.y = (this.bg.height - buttonBg.height) - 30
+    this.checkState()
   }
 
-  onStartClick (): void {
+  async checkState (): Promise<void> {
+    const eth = (window as any).ethereum
+    const provider = new BrowserProvider(eth)
+    const accs = await provider.listAccounts()
+    console.log('accs', accs)
+    if (accs.length > 0) {
+      alert(accs[0].address)
+
+      const startButton = new TapHandler(this.onStartClick.bind(this))
+
+      const buttonBg = new Sprite(Texture.from('UI/elements/button.png'))
+      startButton.addChild(buttonBg)
+
+      startButton.x = (this.bg.width - buttonBg.width) - 20
+      startButton.y = (this.bg.height - buttonBg.height) - 30
+
+      const label = new Text('START', { fill: '0xFFFFFF' })
+      label.anchor = new Point(0.5, 0.5)
+      label.position = new Point(buttonBg.width / 2, buttonBg.height / 2)
+
+      startButton.addChild(label)
+      this.container.addChild(startButton)
+    } else {
+      const connectButton = new TapHandler(this.onConnectClick.bind(this))
+
+      const buttonBg = new Sprite(Texture.from('UI/elements/button.png'))
+      connectButton.addChild(buttonBg)
+
+      connectButton.x = (this.bg.width - buttonBg.width) - 20
+      connectButton.y = (this.bg.height - buttonBg.height) - 30
+
+      const label = new Text('CONNECT', { fill: '0xFFFFFF' })
+      label.anchor = new Point(0.5, 0.5)
+      label.position = new Point(buttonBg.width / 2, buttonBg.height / 2)
+
+      connectButton.addChild(label)
+      this.container.addChild(connectButton)
+    }
+  }
+
+  async onConnectClick (): Promise<void> {
+    const eth = (window as any).ethereum
+    if (eth !== undefined) {
+      const provider = new BrowserProvider(eth)
+
+      await provider.send('eth_requestAccounts', [])
+
+      this.onAccountsChanged(await provider.getSigner())
+    } else {
+      alert('Please Install Metamask!!!')
+    }
+  }
+
+  onAccountsChanged (value: JsonRpcSigner): void {
+    alert(value.address)
+  }
+
+  async onStartClick (): Promise<void> {
     this.callback()
     this.emit('close')
   }
