f0c9f764c772be5d192ede558b90abe9b919cacc
Nick Balyanitsa <nick@sekai.gg>
2023-08-05 12:25:37 +0100

    swc

--- services/evm-connector/dist/middleware/authorizeapi.middleware.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-export var authorizeAPI = function(req, res, next) {
-    if (req.header("X-API-KEY") !== process.env.API_KEY) {
-        res.status(401).json({});
-    } else {
-        next();
-    }
-};
